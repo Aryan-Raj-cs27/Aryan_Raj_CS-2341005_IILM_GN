5b6355f5936623deb5a0e69d59e524bab55a62d5
Aryan Raj <aryan1.raj.cs27@iilm.edu>
2026-08-03 15:05:24 +0530

    Refactor formatting and structure of Solution class

--- BootCamp Training/Codes/Day 1/NumbersWithEvenNumberOfDigits.java
+++ BootCamp Training/Codes/Day 1/NumbersWithEvenNumberOfDigits.java
@@ -1,16 +1,21 @@
-class Solution {
-    public int findNumbers(int[] nums) {
+class Solution 
+{
+    public int findNumbers(int[] nums) 
+    {
         int cnt = 0;
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++)
+        {
             int c = 0;
 
-            while(nums[i] > 0){
+            while(nums[i] > 0)
+            {
                 nums[i] = nums[i] / 10;
                 c++;
             }
 
-            if(c % 2 == 0){
+            if(c % 2 == 0)
+            {
                 cnt++;
             }
         }
