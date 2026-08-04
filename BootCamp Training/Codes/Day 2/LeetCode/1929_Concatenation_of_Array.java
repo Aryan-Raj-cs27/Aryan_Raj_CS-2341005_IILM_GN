e9fb65bef15d760771da30c1d7bcb45967a6f3d6
Aryan Raj <aryan1.raj.cs27@iilm.edu>
2026-08-04 10:56:42 +0530

    Refactor concatenation logic into Solution class

--- BootCamp Training/Codes/Day 2/LeetCode/1929_Concatenation_of_Array.java
+++ BootCamp Training/Codes/Day 2/LeetCode/1929_Concatenation_of_Array.java
@@ -1,23 +1,12 @@
-class a_1929_Concatenation_of_Array {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        int[] result = getConcatenation(nums);
-        
-        System.out.print("Concatenated Array: ");
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
+class Solution 
+{
+   public int[] getConcatenation(int[] nums) 
+   {
+    int[] ans = new int[2*nums.length];
+    for(int i=0; i<nums.length; i++) 
+    {
+        ans[i] = nums[i];
+        ans[i + nums.length] = nums[i];
     }
-    
-    public static int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[2 * n];
-        
-        for (int i = 0; i < n; i++) {
-            result[i] = nums[i];
-            result[i + n] = nums[i];
-        }
-        
-        return result;
-    }
+    return ans;
 }
